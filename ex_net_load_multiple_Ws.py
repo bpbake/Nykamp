9cd871bd5cb05e7a3a403d61cf109d9726fcab84
Brittany <bake0573@umn.edu>
2017-04-26 11:06:24 -0500

    attempt to reset clock to t=0 for each simulation
    there's still an error that needs to be fixed.  read about store() and
    restore() here:
    http://brian2.readthedocs.io/en/stable/reference/brian2.core.html

--- ex_net_load_multiple_Ws.py
+++ ex_net_load_multiple_Ws.py
@@ -31,16 +31,13 @@ import math
 
 start_scope()
 
-N = 1000 #Number of excitatory neurons
+N = 1000 # Number of excitatory neurons
 p_AVG =0.04
 
 tau = 10*ms
 Er = -60*mV
 tauS = 5*ms
 Ee = 0*mV
-#eqs= '''
-#dv/dt = (-v+Er)/tauI : volt
-#'''
 
 eqs= '''
 dv/dt = (-v+Er)/tau: volt
@@ -53,9 +50,7 @@ transienttime = 500*ms
 simulationtime = 1000*ms
 
 #Neuron Groups
-
 G = NeuronGroup(N, eqs, threshold='v>-55*mV', reset='v=-65*mV', refractory='refract', method='euler')
-
 G.v='vreset+(vthreshold-vreset)*rand()'
 
 
@@ -72,35 +67,31 @@ j = 0.2*mV #Weight of neuron connection
 
 S = Synapses(G, G,"w:volt",on_pre='v_post +=w') #Synapse from inhibitory neuron to inhibitory neuron
 S.connect()
+
+store() # record state of simulation for future reference
+
 #SII.connect(condition='i!=j', p=pii)
 
-######Load in matrix
-#L = math.inf
-#alpha_conv=0.3
-#alpha_div=0.3
-#alpha_chain = 0.3
+######Load in matrices one at a time
 start_index = int(input_orig("enter a starting index: "))
 end_index = int(input_orig("enter end index: "))
 
 for w_index in range(start_index, end_index+1):
     
-    filename = "matrices\W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index)
-    #"ws/W_N{0}_L{1}_c{2}_d{3}_ch{4}.pickle".format(NI,L,alpha_conv,alpha_div,alpha_chain)
-    #filename = "ws/W_N{0}_L{1}_c{2}_d{3}_ch{4}.txt".format(NI,L,alpha_conv,alpha_div,alpha_chain)
+    restore() # set the state back to what it was when the store() command was called
     
-    with open(filename, 'rb') as wf:
+    W_filename = "matrices\W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index)
+    #W_filename = "ws/W_N{0}_L{1}_c{2}_d{3}_ch{4}.txt".format(N,L,alpha_conv,alpha_div,alpha_chain)
+    with open(W_filename, 'rb') as wf:
         try:
-            W = pickle.load(wf)
-            #W=numpy.loadtxt(filename)
+            W = pickle.load(wf) # load in W matrix
         except (EOFError):
             print("unpickling error")
     
     stats_filename = "matrices\Stats_W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index)
-    
     with open(stats_filename, 'rb') as sf:
         try:
-            stats = pickle.load(sf)
-            #W=numpy.loadtxt(filename)
+            stats = pickle.load(sf) # load in the stats for the W matrix (L, p_hat, alpha values, alpha_hat values)
         except (EOFError):
             print("unpickling error")
     
@@ -110,30 +101,30 @@ for w_index in range(start_index, end_index+1):
     spikemon = SpikeMonitor(G)
     
     PRM = PopulationRateMonitor(G)
-    
-    
-    
     run(transienttime)
     
-    if spikemon.num_spikes > (transienttime*N/refract*0.5):
-        print("\nnetwork saturated, skipping matrix {0}\n".format(w_index))
-        stats['saturated'] = True
-        result_filename = "matrices\Results_W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index) 
-        with open(result_filename, "wb") as rf:
-            dill.dump(stats, rf)
-        continue
-    if spikemon.num_spikes < (2*N):
-        print("\nnetwork not spiking, skipping matrix {0}\n".format(w_index))
-        stats['not spiking'] = True
-        result_filename = "matrices\Results_W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index) 
-        with open(result_filename, "wb") as rf:
-            dill.dump(stats, rf)
-        continue
-    print("number of spikes in transient: {0}".format(spikemon.num_spikes))
-    #for k,v in stats:
-    #    print(k+":{0}".format(v))
     
-
+    if spikemon.num_spikes > (transienttime*N/refract*0.5): # if the number of spikes it too large, assume it's saturated
+        print("\nnetwork saturated, skipping matrix {0}\n".format(w_index))
+        stats['saturated'] = True # add to the stats dict
+        result_filename = "matrices\Results_W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index) 
+        with open(result_filename, "wb") as rf:
+            dill.dump(stats, rf) #pickle the new stats dict 
+        continue # go to next matrix
+    if spikemon.num_spikes < (2*N): # if the number of spikes is too small, we assume it's not spiking
+        print("\nnetwork not spiking, skipping matrix {0}\n".format(w_index))
+        stats['not spiking'] = True #add to the stats dict
+        result_filename = "matrices\Results_W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index)  
+        with open(result_filename, "wb") as rf:
+            dill.dump(stats, rf) # pickle the new stats file
+        continue # go to next matrix
+    print("\nnumber of spikes in transient: {0}\n".format(spikemon.num_spikes))
+    
+    
+    for k,v in sorted(stats.items()):
+        print(k+":{0}".format(v)) # print the statistics for W
+    print("\n")
+        
     
     # reset before run(simulationtime)
     statemon = StateMonitor(G, 'v', record=0)
@@ -144,7 +135,7 @@ for w_index in range(start_index, end_index+1):
     
     synchrony = analyze_autocor(PRM.rate)
     
-    
+    #plot the results of the simulation
     figure(figsize=(20,10))
     #subplot(122)
     #plot(statemon.t/ms, statemon.v[0])
@@ -160,12 +151,13 @@ for w_index in range(start_index, end_index+1):
     subplot(212)
     plot(PRM.t/ms,PRM.smooth_rate(window='flat', width=0.5*ms)/Hz)
     
-    print("\nExcitatory Synchrony = {}".format(synchrony))
-
+    print("\nExcitatory Synchrony = {0}\n".format(synchrony))
+    
+    # add to the stats dict
     stats['synchrony'] = synchrony
-    stats['Population Rate Monitor'] = PRM.rate/hertz
+    stats['Population Rate Monitor'] = PRM.rate/hertz 
     
     result_filename = "matrices\Results_W_N{0}_p{1}_{2}.pickle".format(N,p_AVG,w_index) 
     with open(result_filename, "wb") as rf:
-       dill.dump(stats, rf)    
+       dill.dump(stats, rf) # pickle the new stats dict
    
